1f17a8d9dcdf3a247c3ab0f386e6f7ab8ab874b8
SamarV-121 <samarvispute121@gmail.com>
2019-04-13 16:47:10 +0000

    RMX1801: Define tap to wake up node
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,6 +78,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno512
 
 # Power
 TARGET_USES_INTERACTION_BOOST := true
+TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
