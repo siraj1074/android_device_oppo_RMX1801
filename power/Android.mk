d4315a87cd1b52c4a2421742ec6f3cdac06a3505
SamarV-121 <samarvispute121@gmail.com>
2019-04-13 16:41:52 +0000

    RMX1801: power: system module plox
    Signed-off-by: SamarV-121 <samarvispute121@gmail.com>

--- power/Android.mk
+++ power/Android.mk
@@ -27,5 +27,4 @@ endif
 LOCAL_MODULE := power.qcom
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
-LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
